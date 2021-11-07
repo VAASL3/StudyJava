796ab6f6b4ceb273ea6bdc311e4946e98d722c0a
VAASL3 <vikialexmur@yandex.ru>
2021-11-07 17:15:10 +0500

    add Second Program

--- src/main/java/Lesson1/FirtstProgram.java
+++ src/main/java/Lesson1/FirtstProgram.java
@@ -1,4 +1,6 @@
-public class FirtstProgramm {
+package Lesson1;
+
+public class FirtstProgram {
 
     public static void main(String[] args) {
         System.out.println("Привет,мир! Hello, world");
